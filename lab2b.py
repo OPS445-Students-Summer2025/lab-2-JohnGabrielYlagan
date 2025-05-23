940653cda0e33d869d7f82d2efea32892fdad5f9
John Gabriel Ylagan <jgylagan@myseneca.ca>
2025-05-23 12:18:26 -0400

    Add lab2a, lab2a1, and lab2b

---
+++ lab2b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: John Gabriel Ylagan
+# lab2b.py
+# Prompt user for name and age and print it
+
+#name = 'Jon'
+#age = 20
+# print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+#colour = input("Type in a colour and press enter: ")
+#print('The colour I typed in is: ' + colour)
+
+name = input("Name: ")
+age = input("Age: ")
+
+print("Hi " + name + ", you are " + age + " years old.")
