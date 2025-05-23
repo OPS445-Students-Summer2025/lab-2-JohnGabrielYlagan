0533d8a622d82c323d8c048614c9e68d4eafa7ee
John Gabriel Ylagan <jgylagan@myseneca.ca>
2025-05-23 16:28:07 -0400

    Lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: John Gabriel Ylagan
+# Author ID: 101563237
+# Date Created: 2025/02/23
+# lab2e.py
+# Use while loop
+
+#count down from
+timer = 10
+
+while timer > 0:
+    print(timer)
+    # print if timer is still greater than 0
+    timer = timer - 1 
+    # will print each instance of timer minus 1 until its 0
+print("blast off!") 
